530fb4d00ba6c90d6a7c37f3258bfda5a142f9b3
Jake Pelletier <ghjnut@gmail.com>
2021-02-17 00:20:47 -0700

    lkizl4 :white_check_mark: Add unit test placeholder

---
+++ lkizl4/test_lkizl4.py
@@ -0,0 +1,11 @@
+import unittest
+
+import lkizl4
+
+class TestLKIZL4(unittest.TestCase):
+
+    def test_main(self):
+        self.assertEqual(True, True)
+
+if __name__ == '__main__':
+    unittest.main()
